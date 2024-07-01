5d555af72f139d517e68e8c45a2e4459b5588995
taehwan heo <htth815@gmail.com>
2024-07-01 11:49:08 +0900

    DOCS: 모듈 info 주석 추가, 아키텍처 부분 수정(database/status -> core/status)

---
+++ src/database/model.py
@@ -0,0 +1,17 @@
+"""
+데이터베이스 테이블에 매핑될 모델 정의(ORM Model)
+"""
+from sqlalchemy import Column, Integer, String, Text, DateTime
+
+from database import Base
+
+'''
+테스트용 테이블
+'''
+class example(Base):
+    __tablename__ = "question"
+
+    id = Column(Integer, primary_key=True)
+    subject = Column(String, nullable=False)
+    content = Column(Text, nullable=False)
+    create_date = Column(DateTime, nullable=False)
