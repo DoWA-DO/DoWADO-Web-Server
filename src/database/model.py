359f51583b8064be18f15b1403e30eb4bccce493
taehwan heo <htth815@gmail.com>
2024-07-02 09:13:25 +0900

    FEAT: 데이터베이스 초기 작성된 코드 pull

--- src/database/model.py
+++ src/database/model.py
@@ -2,8 +2,8 @@
 데이터베이스 테이블에 매핑될 모델 정의(ORM Model)
 """
 from sqlalchemy import Column, Integer, String, Text, DateTime
-
 from .session import Base
+
 
 '''
 테스트용 테이블
--- src/database/session.py
+++ src/database/session.py
@@ -1,7 +1,6 @@
 """
 데이터베이스 세션 연결 및 설정
 """
-
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
