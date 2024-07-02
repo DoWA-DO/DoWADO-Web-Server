bfd4f2837965e9c2ab9b0395004c36fa74635b25
taehwan heo <htth815@gmail.com>
2024-07-02 11:27:27 +0900

    FEAT: src/core 코드작성(cors 미들웨어설정, 전역 에러 초기화, 서버 생명주기 이벤트 초기화, 상태코드 재정의) 및 database docker 비동기 데이터베이스 연동 성공

--- src/database/models.py
+++ src/database/models.py
@@ -1,9 +1,12 @@
 """
 데이터베이스 테이블에 매핑될 모델 정의(ORM Model)
 """
-from sqlalchemy import Column, Integer, String, Text, DateTime
-from .session import Base
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean, Float, JSON, ForeignKey
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+from datetime import datetime
 
+Base = declarative_base()
 
 '''
 테스트용 테이블
