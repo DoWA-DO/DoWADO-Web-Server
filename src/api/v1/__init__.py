581e28b7477ec4a13832ec19ec24e3af7619e340
Polasia <jjong7you@naver.com>
2024-07-03 13:41:52 +0900

    v1 - init 수정 내용 반영

--- src/api/v1/__init__.py
+++ src/api/v1/__init__.py
@@ -1,6 +1,10 @@
 from fastapi import APIRouter
 
 from src.api.v1.example.example_control import router as example_router
+from src.api.v1.teacher.teacher_control import router as teacher_router
 
 router = APIRouter()
 router.include_router(example_router)
+
+teacherrouter = APIRouter()
+teacherrouter.include_router(teacher_router)
