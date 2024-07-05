51582e39fffa2882c8b9bc36b18582a2fede9a87
jeongyeonhwang <wjddus3392@gmail.com>
2024-07-05 11:10:30 +0900

    FEAT: model 저장

--- src/database/model.py
+++ src/database/model.py
@@ -1,7 +1,7 @@
 """
 데이터베이스 테이블에 매핑될 모델 정의(ORM Model)
 """
-from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy import Column, Integer, String, Text, DateTime, Boolean
 from sqlalchemy.dialects.postgresql import TIMESTAMP
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -26,3 +26,15 @@ class Chat(Base):
     chat_text = Column(Text, nullable=True) # 대화 내용
     chat_job = Column(Text, nullable=True) # 직업 (추천 결과)
     chat_date = Column(TIMESTAMP(timezone=True), nullable=False) # 날짜
+    
+class Teacher(Base):
+    __tablename__ = "teacher"
+    
+    teacher_email = Column(String, primary_key=True) # 교원 메일
+    teacher_auth = Column(Boolean, default = False, nullable = False) # 교원 인증 여부
+    teacher_password = Column(String, nullable=False) # 교원 비밀번호
+    teacher_name = Column(String, nullable=False) # 교원 이름
+    teacher_schoolname = Column(String, nullable=False) # 교원 학교 이름
+    #teacher_picture은 기본적으로는 False로 제공, 이후 S3에 해당 계정의 사진이 올라갔는지를 확인하고 True로 바꾸는 로직 필요?
+    #teacher_picture = True면 aws S3에 저장된 사진을 불러오는 로직 필요할것 으로 예상함. - TODO
+    #teacher_picture = Column(Boolean, default = False, nullable = False)
