23d40050e73ba3d77590afa7b4ee10435b6e8966
jeongyeonhwang <wjddus3392@gmail.com>
2024-07-12 11:28:07 +0900

    FEAT: 로그인 API 수정

--- src/api/v1/login/login_dao.py
+++ src/api/v1/login/login_dao.py
@@ -12,20 +12,25 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import joinedload, query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from src.database.model import UserTeacher
+from src.database.model import UserTeacher, UserStudent
 from src.database.session import get_db
 from passlib.context import CryptContext
 
 async def get_user(db: AsyncSession, username: str):
     """
-    Fetch the user from the database and return the teacher password.
+    Fetch the user from the database and return the teacher or student password.
     """
-    result = await db.execute(select(UserTeacher).filter(UserTeacher.teacher_email == username).limit(1))
-    user = result.scalars().first()
-    #hashed_password = [teacher.teacher_password for teacher in user]
-    
-    if user:
-        logging.info(f"Teacher info: {user.teacher_email, user.teacher_password}")
-        return user.teacher_password, user.teacher_email
+    teacher_result = await db.execute(select(UserTeacher).filter(UserTeacher.teacher_email == username).limit(1))
+    teacher = teacher_result.scalars().first()
+
+    student_result = await db.execute(select(UserStudent).filter(UserStudent.student_email == username).limit(1))
+    student = student_result.scalars().first()
+
+    if teacher:
+        logging.info(f"Teacher info: {teacher.teacher_email, teacher.teacher_password}")
+        return teacher.teacher_password, teacher.teacher_email, "teacher"
+    elif student:
+        logging.info(f"Student info: {student.student_email, student.student_password}")
+        return student.student_password, student.student_email, "student"
     else:
-        return None, None
+        return None, None, None
