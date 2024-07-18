c52333192794ede9bebb27ae1cd3a1ee088559b1
jeongyeonhwang <wjddus3392@gmail.com>
2024-07-18 10:29:57 +0900

    FEAT: 파일 업로드

--- src/api/v1/file/file_control.py
+++ src/api/v1/file/file_control.py
@@ -38,32 +38,3 @@ async def upload_board(in_files: List[UploadFile] = File(...)):
 @router.get('/images/{file_name}')
 def get_image(file_name:str):
     return FileResponse(''.join([IMG_DIR,file_name]))
-
-'''
-# File을 그냥 Optional로 지정했음
-async def upload_image(file: UploadFile | None = File(None)):
-    """
-    이미지 업로드 테스트
-    - 1. 클라이언트에서 서버로 이미지를 업로드한다.
-    - 2. 이미지 확장자가 업로드 가능한지 확인한다.
-    - 3. 이미지 사이즈가 업로드 가능한 크기인지 확인한다.
-    - 4. 이미지 이름을 변경한다.
-    - 5. 이미지를 최적화하여 저장한다.
-    """
-    if not file:
-        raise HTTPException(status_code=400, detail="이미지가 제공되지 않았습니다.")
- 
-    file = await images.validate_image_type(file)
-    file = await images.validate_image_size(file)
-    file = images.change_filename(file)
-    filename = file.filename
-    image = images.resize_image(file)
-    image = images.save_image_to_filesystem(image, f"./{filename}")
-    return {"detail": "이미지 업로드 성공"}
- 
-ImageUploader = Annotated[dict, Depends(upload_image)]
- 
-@router.post("/upload")
-async def upload_image(image: dict):
-    return image
-    '''
